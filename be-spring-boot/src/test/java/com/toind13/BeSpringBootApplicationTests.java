e5db0aa45c7a884f0e9e9e928e3d9d77d09a0dfa
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-08-18 10:07:47 +0700

    khoi tao project

---
+++ be-spring-boot/src/test/java/com/toind13/BeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toind13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
